5a7758a535e279aab0163bd260c12643af48ae68
Husnucan Demirci <husnucan.demirci@hotmail.com>
2022-04-16 12:07:22 +0300

    feat: service dropdown added, and some minor changes

--- src/app/service/components/service-main/service-main.component.ts
+++ src/app/service/components/service-main/service-main.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { State } from 'src/app/shared/services/state.service';
 
 @Component({
   selector: 'service-main',
   templateUrl: './service-main.component.html',
   styleUrls: ['./service-main.component.css'],
 })
-export class ServiceMainComponent {}
+export class ServiceMainComponent {
+  string: any;
+  constructor(private router: Router, private state: State) {
+    this.string = this.state.string.service_serviceMain;
+  }
+  titleClicked() {
+    this.router.navigate(['/blog']);
+  }
+}
